df80e400a791bd83e21c67e4ce9850cec642a3bf
Jake Tan <jxketan@gmail.com>
2026-05-16 11:15:26 -0400

    init

--- browser_extension/nara.js
+++
@@ -1,7 +0,0 @@
-const draftMessage = document.getElementById("draftMessage");
-const params = new URLSearchParams(window.location.search);
-const message = params.get("message");
-
-if (message) {
-  draftMessage.textContent = message;
-}
